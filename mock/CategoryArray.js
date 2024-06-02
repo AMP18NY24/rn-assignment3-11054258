d4526e5d793ab9335ea6f365185161f8999f2f9c
AMP18NY24 <nynampadu@st.ug.edu.gh>
2024-06-02 15:37:32 +0000

    final commit

---
+++ mock/CategoryArray.js
@@ -0,0 +1,51 @@
+export const CategoriesData = [
+    {
+        id: 0,
+        name: "Exercise",
+        description: "12 Task",
+        image: require("../assets/woman1.png"),
+    },
+    {
+        id: 1,
+        name: "Study",
+        description: "12 Task",
+        image: require("../assets/woman.png"),
+    },
+    {
+        id: 2,
+        name: "Workout",
+        description: "12 Task",
+        image: require("../assets/woman1.png"),
+    },
+    {
+        id: 3,
+        name: "School",
+        description: "12 Task",
+        image: require("../assets/woman.png"),
+    },
+    {
+        id: 4,
+        name: "Sleep",
+        description: "12 Task",
+        image: require("../assets/woman1.png"),
+    },
+    {
+        id: 5,
+        name: "Surf",
+        description: "12 Task",
+        image: require("../assets/woman.png"),
+    },
+    {
+        id: 6,
+        name: "Dance",
+        description: "12 Task",
+        image: require("../assets/woman1.png"),
+    },
+    {
+        id: 7,
+        name: "Party",
+        description: "12 Task",
+        image: require("../assets/woman.png"),
+    },
+
+];
